ba5f9e3d472a9488dfc363eddcd4124532a8043e
Damiano Gilodi <damiano.gilodi@studenti.unimi.it>
2026-04-08 20:44:07 +0200

    Add directory creation for output path if it doesn't exist

--- analysis/aggregate_datapacket_user.py
+++ analysis/aggregate_datapacket_user.py
@@ -7,22 +7,29 @@ def load_all_simulations(base_logs_path):
     all_data = []
     base_dir = Path(base_logs_path)
 
-    files = list(base_dir.rglob("*.msgpack"))
+    packet_files = list(base_dir.rglob("DataPacket.msgpack"))
 
-    if not files:
+    if not packet_files:
         print(f"Nessun file trovato in {base_logs_path}")
         return pd.DataFrame()
 
-    for file_path in files:
+    for file_path in packet_files:
 
         run_id = file_path.parent.name  # run_0
         app_name = file_path.parent.parent.name  # 1MMM
         scenario = file_path.parent.parent.parent.name  # 1_26_solution_v0
         distribution = file_path.parent.parent.parent.parent.name  # exponential
 
-        with open(file_path, "rb") as f:
-            raw_data = msgpack.unpackb(f.read(), strict_map_key=False)
-            df_temp = pd.DataFrame(raw_data)
+        u_file = file_path.parent / "User.msgpack"
+
+        if u_file.exists():
+            with open(file_path, "rb") as f:
+                df_p = pd.DataFrame(msgpack.unpackb(f.read(), strict_map_key=False))
+
+            with open(u_file, "rb") as f:
+                df_u = pd.DataFrame(msgpack.unpackb(f.read(), strict_map_key=False))
+
+            df_temp = df_p.merge(df_u, left_on="User", right_on="Instance ID", how="left", suffixes=("", "_user"))
 
             df_temp["Distribution"] = distribution
             df_temp["Scenario"] = scenario
@@ -43,4 +50,4 @@ print(full_dataset.head())
 print(f"\nDistribuzioni caricate: {full_dataset['Distribution'].unique()}")
 
 Path("analysis/processed_data").mkdir(parents=True, exist_ok=True)
-full_dataset.to_pickle("analysis/processed_data/full_dataset_raw.pkl")
+full_dataset.to_pickle("analysis/processed_data/dataset_raw_datpacket_user.pkl")
